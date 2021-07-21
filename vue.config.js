bcd20df9f989b6b49a6054031d778dd8df956f5f
developer-spock <developer.spock@gmail.com>
2021-07-21 12:27:06 +0900

    modify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.BASE_URL,
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "",
     outputDir: "./dist",
     indexPath: "index.html",
   }
