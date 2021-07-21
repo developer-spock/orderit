bbb7d66526788fc79118df174489580d4054b2d0
developer-spock <developer.spock@gmail.com>
2021-07-21 13:17:17 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "./" : "",
+    publicPath: process.env.NODE_ENV === 'production' ? '/orderit/' : '/',
     outputDir: "./dist",
     indexPath: "index.html",
     filenameHashing: false,
