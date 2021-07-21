1375163d05281e20b177d328e1c48a32caa203b1
developer-spock <developer.spock@gmail.com>
2021-07-21 13:01:47 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,14 @@ module.exports = {
     css: {
       extract: false
     },
+    chainWebpack: config => {
+      config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => {
+        options.fallback.options.name = "img/[name].[ext]"
+        return options
+      })
+    }
   }
