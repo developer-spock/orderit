b95b002d07e632cc1ed14723298c6e0e74249349
developer-spock <developer.spock@gmail.com>
2021-07-21 12:53:46 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "./" : "",
     outputDir: "./dist",
     indexPath: "index.html",
+    filenameHashing: false,
+    pages: { app: { entry: "src/main.js"}},
+    productionSourceMap: false,
+    css: {
+      extract: false
+    }
   }
