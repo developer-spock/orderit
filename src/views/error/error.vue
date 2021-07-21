9849478013f2d15d56efa2d131c85f559d3003d3
developer-spock <developer.spock@gmail.com>
2021-07-21 12:31:19 +0900

    modify

--- src/views/error/error.vue
+++ src/views/error/error.vue
@@ -3,3 +3,11 @@
     <a :href="href" @click="navigate" :class="{ 'active': isActive }">메인이동</a>
   </router-link>
 </template>
+
+<script>
+export default {
+  mounted: function() {
+    this.$router.push({ name: 'main' });
+  },
+};
+</script>
